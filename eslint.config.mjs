fa867a465531907ab9c1b5cefff0d9a984b4fb64
Jutipong <techcom.ec20@gmail.com>
2024-07-10 21:55:51 +0700

    chore: Update npm dependencies and remove unused files

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu(
+    {
+        stylistic: {
+            indent: 4,
+            overrides: {
+                // 'no-console': 'off',
+                // 'vue/no-console': 'off',
+                'eslint-comments/no-unlimited-disable': 'off',
+            },
+        },
+        typescript: true,
+        vue: true,
+        rules: {
+            'vue/valid-v-slot': ['error', {
+                allowModifiers: true,
+            }],
+            'vue/no-mutating-props': ['error', {
+                shallowOnly: true,
+            }],
+            'vue/prop-name-casing': ['off'],
+            'no-template-shadow': ['off'],
+        },
+        jsonc: false,
+        yaml: false,
+        ignores: [
+            '**/fixtures',
+            'vite-env.d.ts',
+            '**/*.d.ts',
+            '**/.d.ts',
+        ],
+    },
+)
